f0dfe10577fb647d0129dbe6324eb7db3b0930fb
Crazypersonalph <93847055+Crazypersonalph@users.noreply.github.com>
2023-06-27 15:55:19 +0800

    a bit of a hacky work-around for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 // const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const webpack = require('webpack'); //to access built-in plugins
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const path = require('path')
 
 module.exports = {
   mode: 'production',
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/manifest.json', to: './' },
+        { from: './src/icons/', to: './icons/' },
+        { from: './src/inject', to: './inject' },
+        { from: './src/popup', to: './popup' }
+      ]
+    })
+  ],
   entry: {
     seqta: './src/SEQTA.js',
     background: './src/background.js'
