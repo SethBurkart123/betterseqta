e9e87eb66caf168a981c6f985e397c655faaa48e
Crazypersonalph <93847055+Crazypersonalph@users.noreply.github.com>
2023-06-29 14:23:49 +0800

    fixed up some dompurify stuff, working on #40

--- src/SEQTA.js
+++ src/SEQTA.js
@@ -166,14 +166,9 @@ function OpenWhatsNewPopup () {
   <div class="whatsnewFooter">
   <div>
   Report bugs and feedback: 
-  <a href="https://github.com/Nulkem/betterseqta" target="_blank" style="background: none !important; margin: 0 5px; padding:0;"><img style="filter: invert(99%) sepia(0%) saturate(627%) hue-rotate(255deg) brightness(122%) contrast(100%);" height="23" src="${browser.runtime.getURL('/popup/github.svg')}" alt=""></a>
-  <a href="https://chrome.google.com/webstore/detail/betterseqta/boikofabjaholheekefimfojfncpjfib" target="_blank" style="background: none !important; margin: 0 5px; padding:0;">
-  <svg style="width:25px;height:25px" viewBox="0 0 24 24">
-    <path fill="white" d="M12,20L15.46,14H15.45C15.79,13.4 16,12.73 16,12C16,10.8 15.46,9.73 14.62,9H19.41C19.79,9.93 20,10.94 20,12A8,8 0 0,1 12,20M4,12C4,10.54 4.39,9.18 5.07,8L8.54,14H8.55C9.24,15.19 10.5,16 12,16C12.45,16 12.88,15.91 13.29,15.77L10.89,19.91C7,19.37 4,16.04 4,12M15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9A3,3 0 0,1 15,12M12,4C14.96,4 17.54,5.61 18.92,8H12C10.06,8 8.45,9.38 8.08,11.21L5.7,7.08C7.16,5.21 9.44,4 12,4M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
-  </svg>
-  </a>
+  <a href="https://github.com/crazypersonalph/betterseqta" target="_blank" style="background: none !important; margin: 0 5px; padding:0;"><img style="filter: invert(99%) sepia(0%) saturate(627%) hue-rotate(255deg) brightness(122%) contrast(100%);" height="23" src="${browser.runtime.getURL('popup/github.svg')}" alt=""></a>
   </div>
-  <div>Support me: <a href="https://ko-fi.com/O4O5AOFX9" target="_blank" style="background: none !important; margin:0;margin-left:6px; padding:0;"><img height="25" style="border:0px;height:25px;" src="${browser.runtime.getURL('/popup/kofi3.png')}" border="0" alt="Buy Me a Coffee at ko-fi.com"></a></div>
+  <div>Support me: <a href="https://ko-fi.com/crazypersonalph" target="_blank" style="background: none !important; margin:0;margin-left:6px; padding:0;"><img height="25" style="border:0px;height:25px;" src="${browser.runtime.getURL('popup/kofi3.png')}" border="0" alt="Buy Me a Coffee at ko-fi.com"></a></div>
   </div>
   `).firstChild
 
--- src/functions/stringToHTML.js
+++ src/functions/stringToHTML.js
@@ -1,7 +1,7 @@
 import * as DOMPurify from 'dompurify'
 export const stringToHTML = function (str, styles = false) {
   const parser = new DOMParser()
-  str = DOMPurify.sanitize(str, { ADD_ATTR: ['onclick'] })
+  str = DOMPurify.sanitize(str, { ADD_ATTR: ['onclick', 'src'], ALLOW_UNKNOWN_PROTOCOLS: true })
   const doc = parser.parseFromString(str, 'text/html')
   if (styles) { doc.body.style.cssText = 'height: auto; overflow: scroll; margin: 0px; background: var(--background-primary);' }
   return doc.body
